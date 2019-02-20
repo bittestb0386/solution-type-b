1f4d66269fe25427a4155167c44c5818e00ee651
kickscar <kickscar@gmail.com>
2019-02-20 19:08:04 +0900

    init commit

---
+++ src/main/java/problem01/Main.java
@@ -0,0 +1,16 @@
+package problem01;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr = { -10, -3, 5, 6, -20 };
+		findMaxProduct(arr);
+	}
+
+	public static void findMaxProduct(int[] arr) {
+		
+		//
+		// 코드를 완성 하십시오.
+		//
+		
+	}
+}
---
+++ src/main/java/problem03/Money.java
@@ -0,0 +1,30 @@
+package problem03;
+
+public class Money {
+	
+	//
+	// 클래스 구현을 완성 하십시오.
+	// 
+
+	
+	private int amount;
+	
+	public Money(int amount) {
+	}
+
+	public Money add(Money money) {
+		return null;
+	}
+
+	public Money minus(Money money) {
+		return null;
+	}
+
+	public Money multiply(Money money) {
+		return null;
+	}
+
+	public Money devide(Money money) {
+		return null;
+	}
+}
